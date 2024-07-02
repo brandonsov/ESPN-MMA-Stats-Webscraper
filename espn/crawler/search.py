4d1c6723b74d419e41da895da6a2e93f44735f98
Brandon Sov <9397850+brandonsov@users.noreply.github.com>
2024-07-02 11:25:22 -0700

    Add caching to query players

--- espn/crawler/search.py
+++ espn/crawler/search.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import os.path
 import urllib.parse
 
 import requests
@@ -9,12 +11,22 @@ from constants import (
 )
 from helpers import write_file
 
+logging.basicConfig(level=logging.DEBUG)
 
-def query_players(name_query: str):
+
+def query_players(name_query: str) -> str:
+    logging.info(f"Querying player {name_query}")
     query_safe_name = urllib.parse.quote(name_query)
     search_query = f"{ESPN_SEARCH_API_CALL}{query_safe_name}"
+    output_file_name = f"{DATA_CACHE_LOCATION}/{name_query.lower().replace(" ", "_")}{OUTPUT_SEARCH_API_CALL_JSON_FILE_SUFFIX}"
+    if os.path.isfile(output_file_name):
+        logging.info(f"Using cached file {output_file_name}")
+        with open(output_file_name, "r") as response_json:
+            response = response_json.read()
+    else:
+        logging.info(f"Calling GET {search_query}")
+        response = requests.get(search_query)
+        write_file(json.dumps(response.json()), output_file_name, True)
 
-    response = requests.get(search_query)
 
-    output_file_name = f"{DATA_CACHE_LOCATION}/{name_query.replace(" ", "_")}{OUTPUT_SEARCH_API_CALL_JSON_FILE_SUFFIX}"
-    write_file(json.dumps(response.json()), output_file_name, True)
+query_players("Alex Pereira")
